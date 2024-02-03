71624a1d3f95f05eb4357d78a31da5b3ad748abd
Nadia <nlnadialigia@gmail.com>
2024-02-03 17:48:47 -0300

    feat: list tasks and task by id tests

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import requests
 
-BASE_URL = 'http://127.0.0.1:5000'
+BASE_URL = 'http://127.0.0.1:5000/tasks'
 tasks = []
 
 
@@ -10,8 +10,25 @@ def test_create_task():
         "description": "Descrição da Task 01"
     }
 
-    response = requests.post(f'{BASE_URL}/tasks', json=new_task_data)
+    response = requests.post(f'{BASE_URL}', json=new_task_data)
     assert response.status_code == 201
     response_body = response.json()
     assert "id" in response_body
     tasks.append(response_body['id'])
+
+
+def test_get_tasks():
+    response = requests.get(f'{BASE_URL}')
+    assert response.status_code == 200
+    assert 'tasks' in response.json()
+    assert 'total_tasks' in response.json()
+
+
+def test_get_task_by_id():
+    if tasks:
+        task = tasks[0]
+        print(task)
+    response = requests.get(f'{BASE_URL}/{task}')
+    assert response.status_code == 200
+    body = response.json()
+    assert task == body['id']
